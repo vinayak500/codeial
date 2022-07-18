4ad0603d1b5ba86844357838fd1d4e65ac3737fd
vinayak500 <vinayakshetty500@gmail.com>
2022-07-19 00:06:48 +0530

    create controller

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.router();
-
+const homeController = require('../controllers/home_controller');
 console.log("router loaded");
+
+// router.get('/' , homeController.home );
+
+
+
 module.exports = router;
