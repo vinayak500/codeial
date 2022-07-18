b83eb90185fa5221e4a229988749c44d24aca65e
vinayak500 <vinayakshetty500@gmail.com>
2022-07-18 23:57:33 +0530

    create express router

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express router'
+app.use('/' , require('./routers/index'));
+
+
+
+
+
 app.listen(port , function(err){
     if(err){
         // console.log('Error: ' + err);
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.router();
+
+console.log("router loaded");
+module.exports = router;
