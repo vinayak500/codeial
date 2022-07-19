dbbb0c73876f7ac1510dbd288523c118c02f936b
vinayak500 <vinayakshetty500@gmail.com>
2022-07-19 10:36:25 +0530

    added partials for header and footer part

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,8 @@
 module.exports.home = function(req , res){
-          return res.end('<h1> Home Page </h1>')
+        //   return res.end('<h1> Home Page </h1>')
+
+
+        return res.render('home' , {
+                title: "Home"
+        });
 };
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile = function(req , res){
-    return res.end('<h1> Users Profile </h1>')
+    // return res.end('<h1> Users Profile </h1>')
+
+    return res.render('user_profile' , {
+        title: "Home"
+});
 };
