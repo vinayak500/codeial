bcf43107cca9cc0b0696c85a24a05afbc567e7d1
vinayak500 <vinayakshetty500@gmail.com>
2022-07-19 20:43:13 +0530

    creating and altering cookie

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,10 @@
 module.exports.home = function(req , res){
         //   return res.end('<h1> Home Page </h1>')
 
-
+        // cookie data can be altered or changed in both browser or server side
+        //cookies sent from browser to server and back from server to browser
+//      console.log(req.cookies);
+//      res.cookie('user_id' , 25);
         return res.render('home' , {
                 title: "Home"
         });
