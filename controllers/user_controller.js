5cdb7d4b014b8c4ad45cc08595ee0123e4cf2a64
vinayak500 <vinayakshetty500@gmail.com>
2022-07-19 23:59:54 +0530

    sign up code

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,6 @@
+
+const User = require('../models/users');
+
 module.exports.profile = function(req , res){
     // return res.end('<h1> Users Profile </h1>')
 
@@ -25,8 +28,36 @@ module.exports.signIn = function(req,res)
 //get the sign up data
 module.exports.create = function(req ,res)
 {
-     
+     if(req.body.password != req.body.confirm_password)
+     {
+      return res.redirect('back');
+     }
+
+   User.findOne({email:req.body.email} , function(err , user) {
+         if(err){console.log('error in finding user in signing up'); return;
+        }
+
+        if(!user)
+        {
+         User.create(req.body , function(err , user){
+              if(err){console.log('error in creating user while signing up'); return;}
+         
+              return res.redirect('/users/sign-in');
+         
+             });
+        }else{
+         return res.redirect('back');
+        }
+        
+
+   })
+
+   
+
 };
+
+
+
 
 //sign in and create a session for user
 module.exports.createSession = function(req ,res)
