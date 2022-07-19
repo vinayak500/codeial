8c8c70b1ac1a98714f842bd12420814b824488a4
vinayak500 <vinayakshetty500@gmail.com>
2022-07-19 18:20:01 +0530

    linking our mongodb using Mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db = mongoose.connection;
+
+db.on('error' , console.error.bind(console , 'error connecting to database '));
+
+db.once('open' , function(){
+   console.log('connected to mongodb database');
+});
