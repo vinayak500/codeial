1a405877e8b3575880b39070cd8ea0b0a7d6c769
vinayak500 <vinayakshetty500@gmail.com>
2022-07-21 00:11:13 +0530

    setting up scss

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ const passportLocal = require('./config/passport-local-strategy');
 
 const MongoStore = require('connect-mongo');
 
+//node-sass-middleware
+const sassMiddleware = require('node-sass-middleware');
+
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true ,
+    outputStyle: 'extended' ,
+    prefix: '/css' 
+}));
 app.use(express.urlencoded());
 
 app.use(cookieParser());
