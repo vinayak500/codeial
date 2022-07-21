e49375717143ca8164f87d644c5b99acf13fcd65
vinayak500 <vinayakshetty500@gmail.com>
2022-07-21 23:29:30 +0530

    display posta and related user of post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,8 @@
+
+const { populate } = require('../models/post');
+const post = require('../models/post');
+
+
 module.exports.home = function(req , res){
         //   return res.end('<h1> Home Page </h1>')
 
@@ -5,7 +10,24 @@ module.exports.home = function(req , res){
         //cookies sent from browser to server and back from server to browser
 //      console.log(req.cookies);
 //      res.cookie('user_id' , 25);
+
+//        post.find({} , function(err,posts){
+//              return res.render('home' , {
+//                        tittle:'Codeial | Home' ,
+//                        posts : posts
+//              });
+//        });
+
+
+// populate the user of each part
+       post.find({}).populate('user').exec(function(err,posts){
         return res.render('home' , {
-                title: "Home"
-        });
+                title:'Codeial | Home' ,
+                posts : posts
+      });
+       });
+
+        // return res.render('home' , {
+        //         title: "Home"
+        // });
 };
