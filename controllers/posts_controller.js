32ddccb50f629307a591700fdd4765c2c3e257e3
vinayak500 <vinayakshetty500@gmail.com>
2022-07-21 22:51:46 +0530

    saving posts to the DB

---
+++ controllers/posts_controller.js
@@ -0,0 +1,14 @@
+
+const post = require('../models/post');
+
+module.exports.create = function(req , res){
+    post.create({
+        content: req.body.content ,
+        user : req.user._id
+    } , function(err, post){
+        if(err){console.log('error in creating a post'); return; }
+     
+        return res.redirect('back');
+    
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ console.log("router loaded");
 
 router.get('/' , homeController.home );
 router.use('/users' ,require('./users'));
+router.use('/posts' ,require('./posts'));
 
 
 module.exports = router;
