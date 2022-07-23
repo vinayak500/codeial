bdc003951e17c0f11426f2d2c52d91bec5a7e5bb
vinayak500 <vinayakshetty500@gmail.com>
2022-07-23 16:35:32 +0530

    added some styles changes

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -28,7 +28,7 @@ module.exports.home = function(req , res){
 
         return res.render('home' , {
                 title:'Codeial | Home' ,
-                posts : posts
+                posts : posts ,
       });
        });
 
