0e0f45184fa106f639e341992da9704bf96f8549
vinayak500 <vinayakshetty500@gmail.com>
2022-07-25 22:28:54 +0530

    added comments delete feature

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const CommentsController = require('../controllers/comments_controller');
 
 router.post('/create' , passport.checkAuthentication , CommentsController.create);
-
+router.get('/destroy/:id' , passport.checkAuthentication , CommentsController.destroy);
 
 
 module.exports = router;
